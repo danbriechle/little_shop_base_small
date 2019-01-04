93009369ead0f54e3eac0af2d01a289304baf3d5
Daniel Briechle <danbriechle@hotmail.com>
2019-01-03 19:04:32 -0700

    all tests passing

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   get '/logout', to: 'session#destroy'
 
   get '/register', to: 'users#new', as: 'registration'
-  resources :users, param: :slug, only: [:create, :update]
+  resources :users, only: [:create, :update], param: :slug
 
   get '/dashboard', to: 'merchants#show', as: 'dashboard'
   namespace :dashboard do
@@ -34,16 +34,16 @@ Rails.application.routes.draw do
     resources :orders, only: [:index, :create, :show, :destroy]
   end
 
-  post '/admin/users/:merchant_id/items', to: 'dashboard/items#create', as: 'admin_user_items'
-  patch '/admin/users/:merchant_id/items/:id', to: 'dashboard/items#update', as: 'admin_user_item'
+  post '/admin/users/:merchant_slug/items', to: 'dashboard/items#create', as: 'admin_user_items'
+  patch '/admin/users/:merchant_slug/items/:id', to: 'dashboard/items#update', as: 'admin_user_item'
   namespace :admin do
-    resources :users, param: :slug, only: [:index, :show, :edit] do
+    resources :users, only: [:index, :show, :edit], param: :slug do
       patch '/enable', to: 'users#enable', as: 'enable'
       patch '/disable', to: 'users#disable', as: 'disable'
       patch '/upgrade', to: 'users#upgrade', as: 'upgrade'
       resources :orders, only: [:index, :show]
     end
-    resources :merchants, param: :slug, only: [:show] do
+    resources :merchants, only: [:show], param: :slug do
       patch '/enable', to: 'merchants#enable', as: 'enable'
       patch '/disable', to: 'merchants#disable', as: 'disable'
       patch '/upgrade', to: 'merchants#downgrade', as: 'downgrade'
